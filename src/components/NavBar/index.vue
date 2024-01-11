877baf776ce0bd940c4575bcd2fafc9f63e15f73
Bubble <bubble@aratum.cn>
2024-01-11 19:09:08 +0800

    feat: 导航栏组件增加 title

--- src/components/NavBar/index.vue
+++ src/components/NavBar/index.vue
@@ -4,10 +4,19 @@ import { useDarkMode, useToggleDarkMode } from "@/hooks/useToggleDarkMode";
 const onClickRight = () => {
   useToggleDarkMode();
 };
+
+const onClickLeft = () => history.back();
 </script>
 
 <template>
-  <van-nav-bar fixed placeholder @click-right="onClickRight">
+  <van-nav-bar
+    :title="$route.meta.title"
+    :left-arrow="!$route.meta.not_back"
+    fixed
+    placeholder
+    @click-left="onClickLeft"
+    @click-right="onClickRight"
+  >
     <template #right>
       <svg-icon class="text-[18px]" :name="useDarkMode() ? 'light' : 'dark'" />
     </template>
